a7b86cdbd7c0dfd67d3241c38b7dc1a53f65d19e
marina <marina@Users-MacBook-Pro.local>
2020-04-06 14:01:31 +0300

    Updates

---
+++ precache-manifest.403a0398236bd06156369f215df4940d.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e440e9f8991bed92d4a97ea90bd81636",
+    "url": "/exam-monticello/index.html"
+  },
+  {
+    "revision": "02cad814017f69583a0c",
+    "url": "/exam-monticello/static/css/2.34d25fd3.chunk.css"
+  },
+  {
+    "revision": "433a553a19fea330e7d8",
+    "url": "/exam-monticello/static/css/main.16551400.chunk.css"
+  },
+  {
+    "revision": "02cad814017f69583a0c",
+    "url": "/exam-monticello/static/js/2.8b43b6b4.chunk.js"
+  },
+  {
+    "revision": "7b84ee27318968f72cb464bc2fe4d6ec",
+    "url": "/exam-monticello/static/js/2.8b43b6b4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "433a553a19fea330e7d8",
+    "url": "/exam-monticello/static/js/main.fd2ca6e0.chunk.js"
+  },
+  {
+    "revision": "2cc367bee98a539ca69f",
+    "url": "/exam-monticello/static/js/runtime-main.0923c2f1.js"
+  },
+  {
+    "revision": "192881505368610f16aa740bdea4a1df",
+    "url": "/exam-monticello/static/media/Frame.19288150.png"
+  },
+  {
+    "revision": "ca6e213b5621a1a11a11c7f4c65023cd",
+    "url": "/exam-monticello/static/media/GothamBook.ca6e213b.ttf"
+  },
+  {
+    "revision": "f30e83b96507ecc329372146b267541d",
+    "url": "/exam-monticello/static/media/GothamBook.f30e83b9.woff"
+  },
+  {
+    "revision": "4d244f04e92c03f4dbc19dc1bc886573",
+    "url": "/exam-monticello/static/media/Img-1.4d244f04.png"
+  },
+  {
+    "revision": "173b594b60cf4e84e9a9c3354ebe1536",
+    "url": "/exam-monticello/static/media/Img-2.173b594b.png"
+  },
+  {
+    "revision": "f7771850b462157961db99448c67331e",
+    "url": "/exam-monticello/static/media/Img2.f7771850.png"
+  },
+  {
+    "revision": "3ecefff5376c32355d8e211e565b0e2f",
+    "url": "/exam-monticello/static/media/Img3.3ecefff5.png"
+  },
+  {
+    "revision": "15183204e8dbc9bd17e204b6fa60c65f",
+    "url": "/exam-monticello/static/media/Logo.15183204.svg"
+  },
+  {
+    "revision": "1ab65caebc17865365e45f5e774d774b",
+    "url": "/exam-monticello/static/media/MaterialIcons-Regular.1ab65cae.woff"
+  },
+  {
+    "revision": "3febe99def633724384d119b31524874",
+    "url": "/exam-monticello/static/media/MaterialIcons-Regular.3febe99d.woff2"
+  },
+  {
+    "revision": "7be74d583acd2d20383222f4d157880e",
+    "url": "/exam-monticello/static/media/MaterialIcons-Regular.7be74d58.ttf"
+  },
+  {
+    "revision": "b19924217663139208cd7e75907fdafc",
+    "url": "/exam-monticello/static/media/bg-footer.b1992421.png"
+  },
+  {
+    "revision": "52760dc57a6da6ced3ab7c7022da42ed",
+    "url": "/exam-monticello/static/media/facebook-icon.52760dc5.svg"
+  },
+  {
+    "revision": "87677e6a19b78f7bbd13d5f5acf2c2a1",
+    "url": "/exam-monticello/static/media/gothambold1.87677e6a.woff"
+  },
+  {
+    "revision": "c850bb3527a35efc4758fb55da54cf93",
+    "url": "/exam-monticello/static/media/gothambold1.c850bb35.ttf"
+  },
+  {
+    "revision": "2d62019af8681335c779d6e7364b6d14",
+    "url": "/exam-monticello/static/media/image1.2d62019a.png"
+  },
+  {
+    "revision": "6d0bc5d52c5b19d01159109691bf3080",
+    "url": "/exam-monticello/static/media/image2.6d0bc5d5.png"
+  },
+  {
+    "revision": "6b3e1c49f832dd43bc0e06e60d274222",
+    "url": "/exam-monticello/static/media/image3.6b3e1c49.png"
+  },
+  {
+    "revision": "caaea7f6661e5bcf72545030665893e0",
+    "url": "/exam-monticello/static/media/image4.caaea7f6.png"
+  },
+  {
+    "revision": "387b131f137782b90d5a261f80676da4",
+    "url": "/exam-monticello/static/media/image5.387b131f.png"
+  },
+  {
+    "revision": "fe3e6d94328471b7d43da7d43efebd56",
+    "url": "/exam-monticello/static/media/insta-icon.fe3e6d94.svg"
+  },
+  {
+    "revision": "580bf9b0883d1c17a447a11083dfeeab",
+    "url": "/exam-monticello/static/media/monticello-hero-bg.580bf9b0.png"
+  },
+  {
+    "revision": "a78c275558f7c342a412aba2b1c1fd15",
+    "url": "/exam-monticello/static/media/monticello-news-bg.a78c2755.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/exam-monticello/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/exam-monticello/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/exam-monticello/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/exam-monticello/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "44633bcac18b6f88a665404a99921161",
+    "url": "/exam-monticello/static/media/twitter-icon.44633bca.svg"
+  }
+]);
